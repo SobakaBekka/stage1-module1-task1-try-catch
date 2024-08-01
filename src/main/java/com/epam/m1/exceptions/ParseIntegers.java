0f411a5af40a48f9207f3b36ae4ac397a23d9f16
SobakaBekka <st69704@upce.cz>
2024-08-01 16:39:12 +0200

    written  one of catch

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -18,14 +18,20 @@ public class ParseIntegers {
     public static void main(String[] args) {
         Iterator<String> words = WORDS.iterator();
         int sum = 0;
-        String justWords = "";
+        StringBuilder justWords = new StringBuilder();
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
-            // todo: complete it
+            try {
+                int number = Integer.parseInt(next);
+                sum += number;
+            } catch (NumberFormatException e) {
+                if (justWords.length() > 0) {
+                    justWords.append(" ");
+                }
+                justWords.append(next);
+            }
         }
         System.out.println("Sum is " + sum);
-        System.out.println("Just words:" + justWords);
+        System.out.println("Just words: " + justWords.toString());
     }
 }
-
